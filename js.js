49540bcaf2f3e9674c73ce35104f63b6aa3df03e
nicksharma <niksharma1209@gmail.com>
2023-01-03 11:47:18 +0530

    demate

---
+++ js.js
@@ -0,0 +1,21 @@
+function mail() {
+    let a = document.getElementById("userName").value;
+    let b = document.getElementById("contactDetail").value;
+    let c = document.getElementById("gEmail").value;
+    let d = document.getElementById("commentBox").value;
+
+
+    Email.send({
+        Host : "smtp.elasticemail.com",
+        Username : "kajalsingh1000000@gmail.com",
+        Password : "290FE5FEAADCCF8F9DF3FF3CB8B8D9B6316A",
+        To : 'niksharma1209@gmail.com',
+        From : "kajalsingh1000000@gmail.com",
+        Subject : "This is the subject",
+        Body : `${a}  ${b}  ${c} ${d}`
+    }).then(
+      message => alert(message)
+    );
+}
+
+
